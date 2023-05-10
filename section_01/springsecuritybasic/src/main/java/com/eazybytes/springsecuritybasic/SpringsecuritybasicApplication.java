b283c4030d936ac17ac66cfdfbe1074b1f0346d3
danielecaporaletti <danielecaporaletti1@gmail.com>
2023-05-10 20:00:00 +0200

    add comment

--- section_01/springsecuritybasic/src/main/java/com/eazybytes/springsecuritybasic/SpringsecuritybasicApplication.java
+++ section_01/springsecuritybasic/src/main/java/com/eazybytes/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.eazybytes.springsecuritybasic.controller") // Optional
+@ComponentScan("com.eazybytes.springsecuritybasic.controller") // Optional for scanning class outside of subclass of main class
 public class SpringsecuritybasicApplication {
 
 	public static void main(String[] args) {
